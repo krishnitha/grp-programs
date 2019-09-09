946935b29961d3d10e04db9f9f2e2652db308d2f
krishnitha <43699118+krishnitha@users.noreply.github.com>
2019-09-09 17:56:34 +0700

    Update hamiltonian.c

--- hamiltonian.c
+++ hamiltonian.c
@@ -2,20 +2,22 @@
 #define N 100000
 void main()
 {
-	int i,j,a[N];
-	int count,n;
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
-		scanf("%d",&a[i]);
-	for(i=0;i<n;i++){
+	int k,j,a[N];
+	int count,i;
+	scanf("%d",&i);
+	for(k=0;k<i;k++)
+		scanf("%d",&a[k]);
+	for(k=0;k<i;k++)
+	{
 		count=0;
-		for(j=i+1;j<n;j++){
-			if(a[i]>=a[j])
+		for(j=k+1;j<i;j++)
+		{
+			if(a[k]>=a[j])
 				count++;
 			else
 			    break;
 		}
-		if(count==(n-i-1))
-			printf("%d ",a[i]);
+		if(count==(i-k-1))
+			printf("%d ",a[k]);
 	}
 }
